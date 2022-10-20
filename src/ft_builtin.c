8a1052a1a739037ec5a0381ac34e54612d5084ea
Mateo GIRARDOT <mgirardo@e1r5p19.clusters.42paris.fr>
2022-10-20 13:49:45 +0200

    mateal:merge: merging time

--- src/ft_builtin.c
+++ src/ft_builtin.c
@@ -26,7 +26,7 @@ int	ft_cd(t_minishell *minishell, t_command *command)
 {
 	if (ft_strlen(command->cmd) > 2)
 	{
-		write(2, "bash: cd: too many arguments")
+		write(command->ofdout, "minishell: cd: too many arguments\n", ft_strlen("minishell: cd: too many arguments\n"));
 		return (1);
 	}
 	else if (ft_strlen(command->cmd == 2))
@@ -35,8 +35,15 @@ int	ft_cd(t_minishell *minishell, t_command *command)
 		{
 			ft_updateenv(minishell);
 		}
+		else
+		{
+			write (command->ofdout, "minishell: cd: ", ft_strlen("minishell: cd: "));
+			write (command->ofdout, command->cmd[1], ft_strlen(command->cmd[1]));
+			write (command->ofdout, ": No such file or directory\n", ft_strlen(": No such file or directory\n"));
+			return (1);
+		}
 	}
-	else if (ft_strlen(comman->cmd) == 1)
+	else if (ft_strlen(command->cmd) == 1)
 	{
 		if (chdir(/*	$HOME if $home existe sinon retour d'erreur
 						on peut faire ca car defini comme implementation defined
@@ -46,11 +53,17 @@ int	ft_cd(t_minishell *minishell, t_command *command)
 		}
 		else
 		{
-			write(2, "bash: cd: no home defined")
+			write(command->ofdout, "minishell: cd: no home defined\n", ft_strlen("minishell: cd: no home defined\n"));
 			return (1);
 		}
 	}
 	return (0);
+}
+
+void	ft_export(t_minishell *minishell, t_command *command)
+{
+	/* only accept alphanum and underscore to define env variables name */
+	if (ft_strcmp)
 }
 
 void	ft_builtin(t_minishell *minishell, t_command *command)

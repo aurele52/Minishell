e271a051caa411128d0b9d134370834c9129ef2d
Aurele DREYER <audreyer@e1r8p15.clusters.42paris.fr>
2022-11-16 16:47:36 +0100

    norm:ft_signal

--- src/minishell/ft_signal.c
+++ src/minishell/ft_signal.c
@@ -6,24 +6,51 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 19:27:18 by mgirardo          #+#    #+#             */
-/*   Updated: 2022/10/26 19:27:20 by mgirardo         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:46:57 by audreyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//	rl_clear_history
+//		Clear the history list by deleting all of the entries,
+//in the same manner
+//		as the History library’s clear_history() function. This differs from
+//		clear_history because it frees priva
+//	te data Readline saves in the history
+//		list
+//	rl_on_new_line,
+//		Tell the update routines that we have moved onto a new (empty) line,
+//		usually after ouputting a newline
+//	rl_replace_line
+//		Replace the contents of rl_line_buffe
+//	r with text. The point and mark are
+//		preserved, if possible. If clea
+//	r_undo is non-zero, the undo list associated
+//		with the current line is cleared
+//	rl_redisplay
+//		Change what's displayed on the screen to reflect the current contents of
+//		rl_line_buffer
+
+//	if (sig == SIGINT) //ctrl + c
+//	else if (sig == SIGQUIT) //ctrl + /
+
+//	if (sig == SIGINT) // ctrl + c
+//		close (0); //not good enough
+//	else if (sig == SIGQUIT) //ctrl + /
+
 void	ft_signal_main(int sig, siginfo_t *siginfo, void *ucontext)
 {
 	(void)siginfo;
 	(void)ucontext;
-	if (sig == SIGINT) //ctrl + c
+	if (sig == SIGINT)
 	{
 		write(1, "\n", 1);
 		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
 	}
-	else if (sig == SIGQUIT) //ctrl + /
+	else if (sig == SIGQUIT)
 	{
 		write(1, "\33[2K\r", 5);
 		rl_on_new_line();
@@ -35,32 +62,14 @@ void	ft_signalhd(int sig, siginfo_t *siginfo, void *ucontext)
 {
 	(void)siginfo;
 	(void)ucontext;
-	if (sig == SIGINT) // ctrl + c
+	if (sig == SIGINT)
 	{
-		close (0); //not good enough
+		close (0);
 	}
-	else if (sig == SIGQUIT) //ctrl + /
+	else if (sig == SIGQUIT)
 	{
 		write(1, "\33[2K\r", 5);
 		rl_on_new_line();
 		rl_redisplay();
 	}
 }
-
-/*
-	rl_clear_history
-		Clear the history list by deleting all of the entries, in the same manner
-		as the History library’s clear_history() function. This differs from
-		clear_history because it frees private data Readline saves in the history
-		list
-	rl_on_new_line,
-		Tell the update routines that we have moved onto a new (empty) line,
-		usually after ouputting a newline
-	rl_replace_line
-		Replace the contents of rl_line_buffer with text. The point and mark are
-		preserved, if possible. If clear_undo is non-zero, the undo list associated
-		with the current line is cleared
-	rl_redisplay
-		Change what's displayed on the screen to reflect the current contents of
-		rl_line_buffer
-*/
